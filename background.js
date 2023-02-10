9d793e80739a4f6af11157dd6fd0af5624244ccd
rishijain07 <78855205+rishijain07@users.noreply.github.com>
2023-02-11 02:36:22 +0530

    "working code"

--- background.js
+++ background.js
@@ -1,17 +1,33 @@
-var totalDataSent = 0;
-var totalDataReceived = 0;
+// background.js
+
+let dataSent = 0;
+let dataReceived = 0;
 
 chrome.webRequest.onSendHeaders.addListener(
-  function(details) {
-    totalDataSent += details.requestHeaders.map(header => header.value).join("").length;
+  function (details) {
+    dataSent += details.requestHeaders.map(header => header.value).join("").length;
   },
-  {urls: ["<all_urls>"]},
+  { urls: ["<all_urls>"] },
   ["requestHeaders"]
 );
+
 chrome.webRequest.onHeadersReceived.addListener(
-    function(details) {
-      totalDataReceived += details.responseHeaders.map(header => header.value).join("").length;
-    },
-    {urls: ["<all_urls>"]},
-    ["responseHeaders"]
-  );
+  function (details) {
+    dataReceived += details.responseHeaders.map(header => header.value).join("").length;
+  },
+  { urls: ["<all_urls>"] },
+  ["responseHeaders"]
+);
+
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function (msg) {
+    if (msg.type === "getData") {
+      port.postMessage({
+        type: "data",
+        dataSent: dataSent,
+        dataReceived: dataReceived,
+        carbonFootprint: (dataSent + dataReceived) * 0.00043,
+      });
+    }
+  });
+});
--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    chrome.runtime.sendMessage({type: "getData"}, function(response) {
-      document.getElementById("data-sent").textContent = response.totalDataSent;
-      document.getElementById("data-received").textContent = response.totalDataReceived;
-    });
-  });
+// popup.js
+
+const dataSentElement = document.getElementById("data-sent");
+const dataReceivedElement = document.getElementById("data-received");
+const carbonFootprintElement = document.getElementById("carbon-footprint");
+
+const port = chrome.runtime.connect({ name: "popup" });
+
+port.postMessage({ type: "getData" });
+
+port.onMessage.addListener(function (msg) {
+  if (msg.type === "data") {
+    dataSentElement.innerHTML = msg.dataSent + " bytes";
+    dataReceivedElement.innerHTML = msg.dataReceived + " bytes";
+    carbonFootprintElement.innerHTML = msg.carbonFootprint + " kg CO2";
+  }
+});
