c706309772ce1d1809d6658963b40bc5b23534a6
rishijain07 <78855205+rishijain07@users.noreply.github.com>
2023-02-11 16:20:32 +0530

    version 2

--- background.js
+++ background.js
@@ -1,33 +1,47 @@
-// background.js
-
-let dataSent = 0;
-let dataReceived = 0;
+let dataSent = {};
+let dataReceived = {};
+let carbonFootprint = {};
 
 chrome.webRequest.onSendHeaders.addListener(
-  function (details) {
-    dataSent += details.requestHeaders.map(header => header.value).join("").length;
-  },
-  { urls: ["<all_urls>"] },
-  ["requestHeaders"]
+    function (details) {
+        const hostname = new URL(details.url).hostname;
+        if (!dataSent[hostname]) {
+            dataSent[hostname] = 0;
+        }
+        dataSent[hostname] += details.requestHeaders.map(header => header.value).join("").length;
+    },
+    { urls: ["<all_urls>"] },
+    ["requestHeaders"]
 );
 
 chrome.webRequest.onHeadersReceived.addListener(
-  function (details) {
-    dataReceived += details.responseHeaders.map(header => header.value).join("").length;
-  },
-  { urls: ["<all_urls>"] },
-  ["responseHeaders"]
+    function (details) {
+        const hostname = new URL(details.url).hostname;
+        if (!dataReceived[hostname]) {
+            dataReceived[hostname] = 0;
+        }
+        dataReceived[hostname] += details.responseHeaders.map(header => header.value).join("").length;
+        if (!carbonFootprint[hostname]) {
+            carbonFootprint[hostname] = 0;
+        }
+        carbonFootprint[hostname] = (dataSent[hostname] + dataReceived[hostname]) * 0.00043;
+    },
+    { urls: ["<all_urls>"] },
+    ["responseHeaders"]
 );
 
 chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function (msg) {
-    if (msg.type === "getData") {
-      port.postMessage({
-        type: "data",
-        dataSent: dataSent,
-        dataReceived: dataReceived,
-        carbonFootprint: (dataSent + dataReceived) * 0.00043,
-      });
-    }
-  });
+    port.onMessage.addListener(function (msg) {
+        if (msg.type === "getData") {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                let domain = new URL(tabs[0].url).hostname;
+                port.postMessage({
+                    type: "data",
+                    dataSent: dataSent[domain],
+                    dataReceived: dataReceived[domain],
+                    carbonFootprint: carbonFootprint[domain],
+                });
+            });
+        }
+    });
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-// popup.js
-
 const dataSentElement = document.getElementById("data-sent");
 const dataReceivedElement = document.getElementById("data-received");
 const carbonFootprintElement = document.getElementById("carbon-footprint");
@@ -10,8 +8,62 @@ port.postMessage({ type: "getData" });
 
 port.onMessage.addListener(function (msg) {
   if (msg.type === "data") {
-    dataSentElement.innerHTML = msg.dataSent + " bytes";
-    dataReceivedElement.innerHTML = msg.dataReceived + " bytes";
-    carbonFootprintElement.innerHTML = msg.carbonFootprint + " kg CO2";
+    const dataSent = msg.dataSent;
+    const dataReceived = msg.dataReceived;
+    const carbonFootprint = msg.carbonFootprint;
+    dataSentElement.innerHTML = dataSent + " bytes";
+    dataReceivedElement.innerHTML = dataReceived + " bytes";
+    carbonFootprintElement.innerHTML = carbonFootprint + " kg CO2";
+
+    const domainsData = JSON.parse(localStorage.getItem("domainsData")) || {};
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var currentTab = tabs[0];
+      var hostname = new URL(currentTab.url).hostname;
+      const currentDomain = hostname;
+      console.log(currentDomain);
+  
+      if (!domainsData[currentDomain]) {
+        domainsData[currentDomain] = {
+          dataSent,
+          dataReceived,
+          carbonFootprint,
+        };
+      } else {
+        domainsData[currentDomain].dataSent += dataSent;
+        domainsData[currentDomain].dataReceived += dataReceived;
+        domainsData[currentDomain].carbonFootprint += carbonFootprint;
+      }
+  
+      localStorage.setItem("domainsData", JSON.stringify(domainsData));
+  
+    });
+    
+
   }
 });
+
+const tableBody = document.getElementById("table-body");
+
+const renderTable = () => {
+  const domainsData = JSON.parse(localStorage.getItem("domainsData")) || {};
+  tableBody.innerHTML = "";
+  Object.entries(domainsData).forEach(([domain, data]) => {
+    const tr = document.createElement("tr");
+    const domainTd = document.createElement("td");
+    domainTd.innerHTML = domain;
+    const carbonFootprintTd = document.createElement("td");
+    carbonFootprintTd.innerHTML = data.carbonFootprint + " g CO2";
+    const dataSentTd = document.createElement("td");
+    dataSentTd.innerHTML = data.dataSent + " bytes";
+    const dataReceivedTd = document.createElement("td");
+    dataReceivedTd.innerHTML = data.dataReceived + " bytes";
+    tr.appendChild(domainTd);
+    tr.appendChild(carbonFootprintTd);
+    tr.appendChild(dataSentTd);
+    tr.appendChild(dataReceivedTd);
+    tableBody.appendChild(tr);
+  });
+};
+
+window.addEventListener("load", renderTable);
